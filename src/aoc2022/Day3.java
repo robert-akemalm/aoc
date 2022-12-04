dba8c0f359a704848f47e0fc8a7cb646eba6ad91
Robert Åkemalm <robert.akemalm@apptus.com>
2022-12-04 10:54:05 +0100

    Day 4

--- src/aoc2022/Day3.java
+++ src/aoc2022/Day3.java
@@ -1,10 +1,8 @@
 package aoc2022;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Day3 {
     public static void main(String[] args) {

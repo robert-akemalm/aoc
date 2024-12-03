930b4f439818ec1373e98de75098935ac6118dbb
Robert Åkemalm <robert.akemalm@voyado.com>
2024-12-03 23:11:41 +0100

    Day 1-3 2024

---
+++ src/aoc2024/Day.java
@@ -0,0 +1,29 @@
+package aoc2024;
+
+public class Day {
+    private static void a(Input input) {
+
+    }
+
+    private static void b(Input input) {
+
+    }
+
+    record Input() {
+        static Input parse(String input) {
+
+            return new Input();
+        }
+    }
+
+    private static final String TEST_INPUT = "";
+
+    private static final String INPUT = "";
+
+    public static void main(String[] args) {
+        Util.time(()-> a(Input.parse(TEST_INPUT)));
+        Util.time(()->a(Input.parse(INPUT)));
+        Util.time(()->b(Input.parse(TEST_INPUT)));
+        Util.time(()->b(Input.parse(INPUT)));
+    }
+}
